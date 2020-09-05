63de2ecee34599efb635db3505fb5cfc6098d8fd
evrintobing17 <evrintobing@gmail.com>
2020-09-06 02:52:12 +0700

    update function

--- main.go
+++ main.go
@@ -72,18 +72,3 @@ func Login(c *gin.Context) {
 		"token":   tokenString,
 	})
 }
-
-// func CreateToken(username string) (uint64, error) {
-// 	var err error
-
-// 	atClaims := jwt.MapClaims{}
-// 	atClaims["authorized"] = true
-// 	atClaims["username"] = username
-// 	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
-// 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-// 	token, err := at.SignedString([]byte(os.Getenv("chrombit2020")))
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	return token, nil
-// }
